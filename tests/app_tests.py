ad0c47d92e7d26ca4f4e6c1b852aec21adc95485
Chris Schilling <crschilling@gmail.com>
2020-05-03 16:32:24 -0400

    added another test

--- tests/app_tests.py
+++ tests/app_tests.py
@@ -11,4 +11,12 @@ def test_index():
 
     rv = web.get('/', follow_redirects=True)
     assert_equal(rv.status_code, 200)
+
+def test_game():
+    #check that we get a 200 on /game
+    rv = web.get('/game', follow_redirects=True)
+    assert_equal(rv.status_code, 200)
+
+    #check that we have response data on a form sumbit
+    rv = web.post("/game", follow_redirects=True)
     assert_in(b"Central Corridor", rv.data)
