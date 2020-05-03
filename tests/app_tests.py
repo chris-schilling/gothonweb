30b97d357a1d61335641caef98af2c6ea0a53cda
Chris Schilling <crschilling@gmail.com>
2020-05-03 16:21:56 -0400

    Updating tests

--- tests/app_tests.py
+++ tests/app_tests.py
@@ -5,14 +5,10 @@ app.config['TESTING'] = True
 web = app.test_client()
 
 def test_index():
-    rv = web.get('/', follow_redirects=True)
+
+    rv = web.get('/test', follow_redirects=True)
     assert_equal(rv.status_code, 404)
 
-    rv = web.get('/hello', follow_redirects=True)
+    rv = web.get('/', follow_redirects=True)
     assert_equal(rv.status_code, 200)
-    assert_in(b"Fill Out This Form", rv.data)
-
-    data = {'name': 'Chris', 'greet':'Hola'}
-    rv = web.post('/hello', follow_redirects=True, data=data)
-    assert_in(b"Chris", rv.data)
-    assert_in(b"Hola", rv.data)
+    assert_in(b"Central Corridor", rv.data)
