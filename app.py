d8f3ed34203f8e95a4ad2bdf03aed559bad5f366
Chris Schilling <crschilling@gmail.com>
2020-05-03 15:21:19 -0400

    add map, templates, and update app

--- app.py
+++ app.py
@@ -1,31 +1,43 @@
-import os
-from flask import Flask
+from flask import Flask, session, redirect, url_for, escape, request
 from flask import render_template
-from flask import request
-from werkzeug.utils import secure_filename
-
-UPLOAD_FOLDER = 'downloads'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+from gothonweb import planisphere
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/hello", methods=['POST', 'GET'])
+@app.route("/")
 def index():
-    greeting = "Hello World"
+    # this is used to "setup" the session with starting values
+    session['room_name'] = planisphere.START
+    return redirect(url_for("game"))
 
-    if request.method == "POST":
-        name = request.form['name']
-        greet = request.form['greet']
-        greeting = f"{greet}, {name}"
-        file = request.files['file']
-        if file.filename != '':
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return render_template("index.html", greeting=greeting)
+@app.route("/game", methods=['GET', 'POST'])
+def game():
+    room_name = session.get('room_name')
+
+    if request.method == "GET":
+        if room_name:
+            room = planisphere.load_room(room_name)
+            return render_template("show_room.html", room=room)
+        else:
+            # why is there here? do you need it?'
+            return render_template("you_died.html")
     else:
-        return render_template("hello_form.html")
+        action = request.form.get('action')
 
+        if room_name and action:
+            room = planisphere.load_room(room_name)
+            next_room = room.go(action)
+
+            if not next_room:
+                session['room_name'] = planisphere.name_room(room)
+            else:
+                session['room_name'] = planisphere.name_room(next_room)
+
+        return redirect(url_for("game"))
+
+
+# YOU SHOULD CHANGE THIS IF YOU PUT ON THE INTERNET
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 if __name__ == "__main__":
     app.run()
